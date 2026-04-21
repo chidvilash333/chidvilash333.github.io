9c0b08f59f1b07c5daf2a97e0c670ea77ab65a01
chidvilash333 <achidvilash.21bec7148@vitapstudent.ac.in>
2026-04-21 21:56:37 +0000

    Deploying to gh-pages from @ chidvilash333/chidvilash333.github.io@3b447dc876794ef3e3543ed808405f299887606f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -439,16 +439,11 @@ ninja.data = [{
           description: "AI-powered Flask web app for personalized salary negotiation guidance using Groq API",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-intelligent-spell-corrector",
+          title: 'Intelligent Spell Corrector',
+          description: "DP-based spell corrector with Streamlit demo and interactive edit distance table visualizer",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
